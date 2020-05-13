e8ef9c30c4e459d2f88bd28d61a10f5828c5bc31
Joseph Hung <arixxle@gmail.com>
2020-05-13 17:17:04 +0800

    add sent message with enter funtion

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -20,13 +20,47 @@ consumer.subscriptions.create("RoomChannel", {
     if (data.type === 'right') {
       quoteDiv2.innerText = data.content
       quoteDisplay2.appendChild(quoteDiv2)
-      console.log(data.content);
+      // console.log(data.content);
     } else {
       quoteDiv1.innerText = data.content
       quoteDisplay1.appendChild(quoteDiv1)
-      console.log(data.content);
+      // console.log(data.content);
     }
 
 
   }
 });
+var submitMsg;
+
+
+window.onload = function () {
+  var quoteInputLeft = document.querySelector('#quoteInput');
+  var quoteInputRight = document.querySelector('#quoteInput2');
+  var submitBtnLeft = document.querySelector('input[name="left_input"]')
+  var submitBtnRight = document.querySelector('input[name="right_input"]')
+
+  function typeInputL(e) {
+    e.preventDefault();
+    console.log(quoteInputLeft.value);
+
+    if (e.keyCode === 13) {
+      submitBtnLeft.click();
+      quoteInputLeft.value = ''
+    }
+  }
+  function typeInputR(e) {
+    e.preventDefault();
+    console.log(quoteInputRight.value);
+
+    if (e.keyCode === 13) {
+      submitBtnRight.click();
+      quoteInputRight.value = ''
+
+    }
+  }
+
+  quoteInputLeft.addEventListener('change', typeInputL)
+  quoteInputLeft.addEventListener('keyup', typeInputL)
+  quoteInputRight.addEventListener('change', typeInputR)
+  quoteInputRight.addEventListener('keyup', typeInputR)
+}
