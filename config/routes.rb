6a9881ad482eb56e7606c8e79fa9f74ccbc0d845
Joseph Hung <arixxle@gmail.com>
2020-05-13 12:50:05 +0800

    add rooms index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/typearea', to: "typing#area"
+  root to: 'rooms#index'
+
+  resources :rooms
 end
