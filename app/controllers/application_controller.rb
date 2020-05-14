22c4cc5ecab5ce5d2c03b34cf77bab6dcd167f39
Joseph Hung <arixxle@gmail.com>
2020-05-14 17:52:44 +0800

    add hero and connect hero to message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_hero
+
+  def current_hero
+    return @current_hero if @current_hero.present?
+
+    if session[:hero_id].present?
+      @current_hero = Hero.find(session[:hero_id])
+    else
+      @current_hero = Hero.generate
+      session[:hero_id] = @current_hero.id
+      @current_hero
+    end
+  end
 end
--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,2 +1,10 @@
 class Hero < ApplicationRecord
+  validates_uniqueness_of :heroname
+  def self.generate
+    adjectives = ['稀有的', '瘋狂的', '憂鬱的', '喜悅的', '悲傷的', '傳奇的']
+    nouns = ['布丁', '雞腿', '牛排', '雪糕', '黑輪']
+    number = rand.to_s[2..4]
+    heroname = "#{adjectives.sample}-#{nouns.sample}-#{number}"
+    create(heroname: heroname)
+  end
 end
